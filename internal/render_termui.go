c60d41c0c1bc15d56a23231237e47bb7a49b37e5
yeqown <yeqown@gmail.com>
2021-10-22 14:45:43 +0800

    feat(renderer/termui): add arrow between local and remote list

--- internal/render_termui.go
+++ internal/render_termui.go
@@ -3,7 +3,10 @@ package internal
 import (
 	"errors"
 	"fmt"
-	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
 	"strconv"
 
 	ui "github.com/gizak/termui/v3"
@@ -34,10 +37,17 @@ var (
 // wait any key break view loop
 func (t termuiRenderer) wait() (err error) {
 	defer ui.Close()
+
 	for e := range ui.PollEvents() {
+		log.WithField("event", e).Debug("an event received")
 		if e.Type == ui.KeyboardEvent {
 			if e.ID == "<Enter>" {
 				return errEnter
+			}
+			if e.ID == "<C-c>" {
+				ui.Close()
+				os.Exit(0)
+				return
 			}
 			break
 		}
@@ -49,8 +59,8 @@ func (t termuiRenderer) wait() (err error) {
 func (t termuiRenderer) renderingDiffs(diffs []diff1) decide {
 	t.initUI()
 	x, y := ui.TerminalDimensions()
-	p := widgets.NewParagraph()
 
+	p0 := widgets.NewParagraph()
 	layout := `        AppID: %s
           Env: %s
        luster: %s
@@ -58,49 +68,81 @@ PortalAddress: %s
   AutoPublish: %v
         Force: %v
     Overwrite: %v`
-
-	p.Text = fmt.Sprintf(layout, t.scope.ApolloAppID, t.scope.ApolloEnv, t.scope.ApolloClusterName,
+	p0.Text = fmt.Sprintf(layout, t.scope.ApolloAppID, t.scope.ApolloEnv, t.scope.ApolloClusterName,
 		t.scope.ApolloPortalAddr, t.scope.ApolloAutoPublish, t.scope.Force, t.scope.Overwrite,
 	)
-	p.Border = true
-	p.Title = "synchronize settings"
-	p.TitleStyle = ui.NewStyle(ui.ColorCyan)
-	p.SetRect(0, 0, x, y/4+1)
+	p0.Border = true
+	p0.Title = "synchronize settings"
+	p0.TitleStyle = ui.NewStyle(ui.ColorCyan)
+	p0.SetRect(0, 0, x/2+1, y/4+1)
+
+	p1 := widgets.NewParagraph()
+	p1.Text = "Press [Enter] to continue, [^C] to quit, other keys to cancel"
+	p1.Title = "Tips"
+	p1.TitleStyle = ui.NewStyle(ui.ColorCyan)
+	//p1.TextStyle =
+	p1.SetRect(x/2+1, 0, x, y/4+1)
 
 	l1 := widgets.NewList()
 	l1.Title = "Local"
-	l1.TitleStyle = ui.NewStyle(ui.ColorMagenta)
+	l1.TitleStyle = ui.NewStyle(ui.ColorCyan)
+	l1.TextStyle = ui.NewStyle(ui.ColorCyan)
+	l1.SelectedRow = 9
+
 	l2 := widgets.NewList()
 	l2.Title = "Remote"
-	l2.TitleStyle = ui.NewStyle(ui.ColorBlue)
+	l2.TitleStyle = ui.NewStyle(ui.ColorCyan)
+	l2.TextStyle = ui.NewStyle(ui.ColorCyan)
+	l2.SelectedRow = 9
+
 	l3 := widgets.NewList()
-	l3.TitleStyle = ui.NewStyle(ui.ColorCyan)
 	l3.Title = "Absolute Path"
+	l3.TitleStyle = ui.NewStyle(ui.ColorCyan)
+	l3.TextStyle = ui.NewStyle(ui.ColorWhite)
+	l3.SelectedRow = 9
 
-	dire := widgets.NewImage(directionImage(x/6+1, 2*y/4+1, 2*x/6+1, 3*y/4+1, t.scope.Mode))
-	dire.Border = false
+	//img := widgets.NewImage(directionImage(0, 0, x/6-2, y/4-1, t.scope.Mode))
+	//img.Border = false
 
-	for _, d := range diffs {
-		l3.Rows = append(l3.Rows, d.absFilepath)
+	p2 := widgets.NewParagraph()
+	p2.Border = false
+	switch t.scope.Mode {
+	case SynchronizeMode_DOWNLOAD:
+		p2.Text = `      ⬅️⬅️`
+
+	case SynchronizeMode_UPLOAD:
+		p2.Text = "      ➡️➡️"
+	}
+
+	for idx, d := range diffs {
+		row1, row2 := d.key, d.key
+		color1, color2 := "yellow", "yellow"
+
 		switch d.mode {
 		case diffMode_CREATE:
-			l1.Rows = append(l1.Rows, d.key)
-			l2.Rows = append(l2.Rows, "-")
+			row2 = "✖✖✖"
+			color1 = "green"
+			color2 = "green"
 		case diffMode_DELETE:
-			l1.Rows = append(l1.Rows, "-")
-			l2.Rows = append(l2.Rows, d.key)
+			row1 = "✖✖✖"
+			color1 = "red"
+			color2 = "red"
 		case diffMode_MODIFY:
-			l1.Rows = append(l1.Rows, d.key)
-			l2.Rows = append(l2.Rows, d.key)
 		}
+
+		row1 = fmt.Sprintf("[%d] [%s %s](fg:%s)", idx+1, d.mode, row1, color1)
+		row2 = fmt.Sprintf("[%d] [%s %s](fg:%s)", idx+1, d.mode, row2, color2)
+		l1.Rows = append(l1.Rows, row1)
+		l2.Rows = append(l2.Rows, row2)
+		l3.Rows = append(l3.Rows, d.absFilepath)
 	}
 
 	l1.SetRect(0, y/4+1, x/6+1, y)
-	dire.SetRect(x/6+1, 2*y/4+1, 2*x/6+1, 3*y/4+1)
+	p2.SetRect(x/6+1, 2*y/4+1, 2*x/6+1, 3*y/4+1)
 	l2.SetRect(2*x/6+1, y/4+1, 3*x/6+1, y)
 	l3.SetRect(3*x/6+1, y/4+1, x, y)
 
-	ui.Render(p, l1, dire, l2, l3)
+	ui.Render(p0, p1, l1, p2, l2, l3)
 	if err := t.wait(); err != nil {
 		if errors.Is(err, errEnter) {
 			return Decide_CONFIRMED
@@ -119,6 +161,7 @@ func (t termuiRenderer) renderingResult(results []*synchronizeResult) {
 	tb.TitleStyle = ui.NewStyle(ui.ColorCyan)
 	tb.Border = true
 	tb.RowSeparator = true
+	tb.ColumnWidths = []int{6, 20, 20, 20, x - 66}
 	tb.TextAlignment = ui.AlignCenter
 	tb.FillRow = true
 	tb.Rows = [][]string{
@@ -137,18 +180,11 @@ func (t termuiRenderer) renderingResult(results []*synchronizeResult) {
 
 		tb.RowStyles[idx+1] = ui.NewStyle(ui.ColorGreen)
 		if !r.succeeded {
-			tb.RowStyles[idx+1] = ui.NewStyle(ui.ColorWhite, ui.ColorRed)
+			tb.RowStyles[idx+1] = ui.NewStyle(ui.ColorRed, ui.ColorBlack, ui.ModifierBold)
 		}
 	}
 	tb.SetRect(0, 0, x, y)
 
 	ui.Render(tb)
 	_ = t.wait()
-}
-
-// draw arrow direction (left arrow, right arrow) image
-func directionImage(x1, y1, x2, y2 int, mode SynchronizeMode) image.Image {
-	img := image.NewGray(image.Rect(x1, y1, x2, y2))
-	// TODO(@yeqown) draw the arrow
-	return img
 }
